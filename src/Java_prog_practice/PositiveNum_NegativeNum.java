e8ef55c2ba3ab7b4621d703cca86e94b0101cf41
ADMIN <ADMIN@DESKTOP-9739V97>
2023-11-28 23:16:55 +0530

    cm

---
+++ src/Java_prog_practice/PositiveNum_NegativeNum.java
@@ -0,0 +1,30 @@
+package Java_prog_practice;
+
+
+public class PositiveNum_NegativeNum 
+{
+
+public static void main(String[] args) 
+{
+		// TODO Auto-generated method stub
+
+
+//number to be check  
+int num=23;  
+//checks the number is greater than 0 or not  
+if(num>0)  
+{  
+System.out.println("The number is positive.");  
+}  
+//checks the number is less than 0 or not  
+else if(num<0)  
+{  
+System.out.println("The number is negative.");  
+}  
+
+else  
+{  
+System.out.println("The number is zero.");  
+ }  
+}
+}
