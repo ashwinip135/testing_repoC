08d4aeedd4bb578007bfacad771e3275e05daf79
ADMIN <ADMIN@DESKTOP-9739V97>
2023-11-30 10:51:17 +0530

    cm

---
+++ src/Java_prog_practice/Object_class.java
@@ -0,0 +1,72 @@
+package Java_prog_practice;
+
+public class Object_class {
+
+}
+
+class Mobile {
+	String name;
+	int price;
+	String OS;
+	int storage;
+	void calling() {
+	
+	}
+}
+class NewProject{
+	String ProjectName;
+	int number_of_people;
+	int completionDate;
+	 
+	public void execution() {
+		
+	}
+	
+}
+
+class classroom{
+   int student;
+   String name;
+   int standard;
+   String section;
+   
+    void study() {
+    	
+    }
+}
+
+ class House{
+	 int plotNo ;
+	 int area;
+	 String name;
+	 
+	 void stay() {
+	 }
+	 }
+ class fan{
+	 int price;
+	 int speed;
+	 String shape;
+	 String name;
+	 
+	 void cooling() {
+	 }
+	}
+	 
+	 
+ class bycycle{
+	 int price;
+	 int weight;
+	 void exercise(){
+	
+	 }
+ }
+	
+  class Key{
+	 int size;
+	 String shape;
+	 void Open_Door() {
+	 }
+	 }
+	 
+
