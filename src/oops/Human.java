70c0518be6e1efbd3ee6f6c393fa9e61ac0b7215
ADMIN <ADMIN@DESKTOP-9739V97>
2023-12-11 22:29:30 +0530

    cu_human

---
+++ src/oops/Human.java
@@ -0,0 +1,26 @@
+package oops;
+
+public class Human {
+
+		String name;
+		int age;
+		static String country;
+		static int pin; 
+		
+		void speak(String content) {
+			System.out.println( name + ": " + content);
+		}
+			
+			void happybirthday() {
+				age ++;
+				System.out.println("Happy BDay" + " " + name);
+				if(age == 18);{
+				System.out.println("congrts today you become adult category");
+				}
+			}
+		
+		}
+
+
+
+
