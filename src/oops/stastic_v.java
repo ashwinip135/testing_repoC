0c9cac3500035c0f1994af63532abda9c511a4f9
ADMIN <ADMIN@DESKTOP-9739V97>
2023-12-11 22:30:37 +0530

    static_variable

---
+++ src/oops/stastic_v.java
@@ -0,0 +1,33 @@
+package oops;
+
+public class stastic_v {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human hu1 =new Human();
+		hu1.age=45;
+		hu1.name="Lashamn";
+		hu1.country ="Spain";
+		
+		Human hu2 =new Human();
+		hu2.country ="India";
+		
+		System.out.println(hu1.country);
+		
+		Human hu3 = new Human();
+		hu3.age =40;
+		hu3.name = "Patil";
+		hu3.country = "UK";
+		hu3.pin =1234;
+		System.out.println(hu3.pin);
+		
+		Human hu4 =new Human();
+		hu4.age =56;
+		hu4.name ="Geeta";
+		System.out.println(Human.pin);
+		
+		
+
+	}
+
+}
